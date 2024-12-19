53e680b1fc9eb46cd02528bb1ac5e7a258f0180d
Miha Prajs <108951795+mihaprajs@users.noreply.github.com>
2024-12-19 10:18:18 +0100

    update

--- practice/2024-12-19.R
+++ practice/2024-12-19.R
@@ -5,8 +5,19 @@ data <- read.delim("practice/data.txt")
 model1 <- lm(data$Weight~data$Height,data)
 summary(model1)
 
-model2 <- lm(data$Height~data$Shoe.size,data)
+model2 <- lm(data$Shoe.size~data$Height,data)
 summary(model2)
 
-model3 <- lm(data$Height~data$Sex)
+model3 <- lm(data$TV..hours.per.week.~data$Internet..hours.per.week.,data)
 summary(model3)
+
+## Naloga 2
+plot(x=data$Height,y=data$Weight)
+abline(model1)
+
+library(ggplot2)
+ggplot(data,aes(x=data$Height,y=data$Weight)) +
+  geom_point() +
+  stat_smooth(method="lm",col="red")
+
+## Naloga 3
